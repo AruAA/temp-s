e4a2f0b6c7fb8f83f9092cd4549c30c685eb59db
Aryan Arora <arora.aryan7@gmail.com>
2023-03-16 20:40:51 +0100

    remove more packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES :=  Drive Maps MyVerizonServices SprintDM SprintHM YouTube YouTubeMusicPrebuilt GoogleFeedback RecorderPrebuilt Videos Ornament Photos Tycho GoogleFeedback GoogleCamera MyVerizonServices SprintDM SprintHM PrebuiltGmail OBDM_Permissions OemDmTrigger Snap Camera2 ScribePrebuilt Showcase SoundAmplifierPrebuilt SafetyHubPrebuilt PixelLiveWallpaperPrebuilt  WallpapersBReel2020 arcore DevicePolicyPrebuilt MaestroPrebuilt ConnMO ConnMetrics DMService VZWAPNLib VzwOmaTrigger SCONE USCCDM Papers CalendarGooglePrebuilt CalendarGoogle NgaResources TetheringEntitlement BetterBug
+LOCAL_OVERRIDES_PACKAGES :=  Drive Maps MyVerizonServices SprintDM SprintHM YouTube YouTubeMusicPrebuilt GoogleFeedback RecorderPrebuilt Videos Ornament Photos Tycho GoogleFeedback GoogleCamera MyVerizonServices SprintDM SprintHM PrebuiltGmail OBDM_Permissions OemDmTrigger Snap Camera2 ScribePrebuilt Showcase SoundAmplifierPrebuilt SafetyHubPrebuilt PixelLiveWallpaperPrebuilt  WallpapersBReel2020 arcore DevicePolicyPrebuilt MaestroPrebuilt ConnMO ConnMetrics DMService VZWAPNLib VzwOmaTrigger SCONE USCCDM Papers CalendarGooglePrebuilt CalendarGoogle NgaResources TetheringEntitlement BetterBug Chrome Chrome-Stub TrichromeLibrary-Stub TrichromeLibrary
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
